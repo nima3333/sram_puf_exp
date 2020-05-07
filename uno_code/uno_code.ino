530b769e5d12b7caaee5d8b236cc6866a1e3e122
Nima <nimajaune@gmail.com>
2020-05-07 20:08:57 +0200

    modified uno code to support dac

--- uno_code/uno_code.ino
+++ uno_code/uno_code.ino
@@ -1,11 +1,25 @@
 #include <SoftwareSerial.h>
+#include <Wire.h>
+
+#define MCP4725_ADDR 0x60   
 
 SoftwareSerial mySerial(3, 4);
 int i = 0;
 int compteur = 0;
 
+void DAC_control(int value){
+  Wire.beginTransmission(MCP4725_ADDR);
+  Wire.write(64);                     // cmd to update the DAC
+  Wire.write(value >> 4);        // the 8 most significant bits...
+  Wire.write((value & 15) << 4); // the 4 least significant bits...
+  Wire.endTransmission();
+}
+
 void setup() {
   Serial.begin(57600);
+  Wire.begin();
+  DAC_control(0);
+  
   Serial.println(F("How many rounds"));
   while(!Serial.available()){
     delay(100);
@@ -13,27 +27,30 @@ void setup() {
   if (Serial.available()){
     i = Serial.parseInt();
     Serial.read();
+    Serial.println(i);
   }
   delay(1000);
+  
   mySerial.begin(57600);
-  pinMode(8, OUTPUT);
-  digitalWrite(8, HIGH);
+  DAC_control(4095);
 }
 
 void loop() {
   if (mySerial.available()){
     char inByte = mySerial.read();
-    if(inByte!='X')
+    if(inByte!='X'){
       Serial.write(inByte);
+    }
     else{
       char inByte = mySerial.read();
       if(inByte=='X'){
-        digitalWrite(8, LOW);
+        DAC_control(0);
+
         if(++compteur == i){
           for (;;);
         }
-        delay(10000);
-        digitalWrite(8, HIGH);
+        delay(4000);
+        DAC_control(4095);
       }
     }
   }
