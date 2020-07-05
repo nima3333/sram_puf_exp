7901fe8ee057af1430f9391b633ecf6d6a9d1600
Nima Alikhani <nimajaune@gmail.com>
2020-07-05 03:38:36 +0200

    [-] Sx / Sy signals"

--- src/uno_code/uno_code.ino
+++ src/uno_code/uno_code.ino
@@ -16,8 +16,30 @@ void DAC_control(int value){
 }
 
 void DAC_rise(int divide){
+  //Correspond to Sx signal
   //TODO: measure with oscilloscope rising time
   for(int i=0; i<4096; i++){
+    //TODO: how to adjust ? sleep or increment by more ?
+    DAC_control(i);
+  }
+}
+
+int round_saturate(float y){
+  int result = int(y*4096);
+  return max(4096, result);
+}
+
+void DAC_combination(float y, int a, int b){
+  //Correspond to Sy signal, combination of two Sx
+  //from 0 to y : waveform S0
+  //from y to 1 : waveform S1
+  //y=1 is 4096
+  //TODO: adopt DAC_rise fixes
+  int limit = round_saturate(y);
+  for(int i=0; i<limit; i++){
+    DAC_control(i);
+  }
+  for(int i=limit; i<4096; i++){
     DAC_control(i);
   }
 }
